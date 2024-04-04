77a6eecdd0870ffbc1d13770b45f9389133eba26
Kristin Obermyer <ms-O@users.noreply.github.com>
2024-04-04 12:37:58 -0400

    Add second message

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -3,6 +3,7 @@ public class CurrencyConverter {
 
     public static void main(String[] args) {
         System.out.println("Hello Currency Converter 002");
+        System.out.println("Add line from dev");
     }
 
 }
